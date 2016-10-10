4a4e7b22c74a5d14fb366c428f95fcbc88bc4097
mattwisniewski <matthew.wisniews@gmail.com>
2016-10-10 11:19:55 -0500

    Add files via upload
    Finished

--- Lab14/Lab14a-ArrayFunHouse1/ArrayFunHouse.java
+++ Lab14/Lab14a-ArrayFunHouse1/ArrayFunHouse.java
@@ -2,35 +2,32 @@
  * ArrayFunHouse.java  10/7/2016
  *
  * @author - Matthew Wisniewski
- * @author - Period 8
+ * @author - Period 4
  * @author - Id 859732
  *
  * @author - I received help from Spencer Sharp
  *
 */
-import java.util.Arrays;
 public class ArrayFunHouse
 {
-	//instance variables and constructors could be used, but are not really needed
-	
-	//getSum() will return the sum of the numbers from start to stop, not including stop
+	//Returns the sum of the numbers from start to stop, not including stop
 	public static int getSum(int[] nums, int start, int stop)
 	{
 		int sum = 0;
-		for(int i = start; i < stop; i++)
+		for (int i = start; i <= stop; i++)
 		{
 			sum += nums[i];
 		}
 		return sum;
 	}
 
-	//getCount() will return number of times val is present
+	//Returns the number of times val is in array
 	public static int getCount(int[] nums, int val)
 	{
 		int count = 0;
-		for(int item : nums) 
-		{ 
-			if (item == val) 
+		for (int item : nums)
+		{
+			if (item == val)
 			{
 				count++;
 			}
@@ -38,15 +35,17 @@ public class ArrayFunHouse
 		return count;
 	}
 
+	//Removes the val from array and creates new array without val
 	public static int[] removeVal(int[] nums, int val)
 	{
-		int numDel = getCount(nums,7);
+		int numDel = getCount(nums, 7);
+		int count = 0;
 		int[] newray = new int[nums.length - numDel];
-		for(int item : nums)
+		for (int i = 0; i < nums.length; i++)
 		{
-			if (item != val)
+			if (nums[i] != val)
 			{
-				
+				newray[count++] = nums[i];
 			}
 		}
 		return newray;
--- Lab14/Lab14a-ArrayFunHouse1/ArrayFunHouseRunner.java
+++ Lab14/Lab14a-ArrayFunHouse1/ArrayFunHouseRunner.java
@@ -2,7 +2,7 @@
  * ArrayFunHouseRunner.java  10/7/2016
  *
  * @author - Matthew Wisniewski
- * @author - Period 8
+ * @author - Period 4
  * @author - Id 859732
  *
  * @author - I received help from no one
@@ -12,20 +12,26 @@ import java.util.Arrays;
 
 public class ArrayFunHouseRunner
 {
-	public static void main( String args[] )
+	public static void main(String[] args)
 	{
-		int[] one = {4,10,0,1,7,6,5,3,2,9};
-
+		int[] one = {7, 4, 10, 0, 1, 7, 6, 5, 3, 2, 9, 7};
 		System.out.println(Arrays.toString(one));
-		System.out.println("sum of spots 3-6  =  " + ArrayFunHouse.getSum(one,3,6));
-		System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(one,2,9));
-		System.out.println("# of 4s  =  " + ArrayFunHouse.getCount(one,4));
-		System.out.println("# of 9s  =  " + ArrayFunHouse.getCount(one,9));
+		System.out.println("sum of spots 3-6  =  " + ArrayFunHouse.getSum(one, 3, 6));
+		System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(one, 2, 9));
+		System.out.println("# of 4s  =  " + ArrayFunHouse.getCount(one, 4));
+		System.out.println("# of 9s  =  " + ArrayFunHouse.getCount(one, 9));
+		System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(one, 7));
+		System.out.println("new array with all 7s removed = " + Arrays.toString(ArrayFunHouse.removeVal(one, 7)));
+		System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(ArrayFunHouse.removeVal(one, 7), 7));
 
-		int[] two = {4,2,3,4,6,7,8,9,0,10,0,1,7,6,5,3,2,9,9,8,7};
-
-		//add test cases
-
-
+		int[] two = {7, 4, 2, 7, 3, 4, 6, 7, 8, 9, 7, 0, 10, 7, 0, 1, 7, 6, 5, 7, 3, 2, 7, 9, 9, 8, 7};
+		System.out.println("\n\n" + Arrays.toString(two));
+		System.out.println("sum of spots 3-6  =  " + ArrayFunHouse.getSum(two, 3, 16));
+		System.out.println("sum of spots 2-9  =  " + ArrayFunHouse.getSum(two, 2, 9));
+		System.out.println("# of 0s  =  " + ArrayFunHouse.getCount(two, 0));
+		System.out.println("# of 3s  =  " + ArrayFunHouse.getCount(two, 3));
+		System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(two, 7));
+		System.out.println("new array with all 7s removed = " + Arrays.toString(ArrayFunHouse.removeVal(two, 7)));
+		System.out.println("# of 7s  =  " + ArrayFunHouse.getCount(ArrayFunHouse.removeVal(two, 7), 7));
 	}
 }

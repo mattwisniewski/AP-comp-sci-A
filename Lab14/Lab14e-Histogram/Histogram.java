e67b9487875169452d9567094a6f84ab01de3191
mattwisniewski <matthew.wisniews@gmail.com>
2016-10-10 11:19:17 -0500

    Add files via upload
    Finished histogram

--- Lab14/Lab14e-Histogram/Histogram.java
+++ Lab14/Lab14e-Histogram/Histogram.java
@@ -1,4 +1,13 @@
-
+/**
+ * ArrayFunHouse.java  10/7/2016
+ *
+ * @author - Matthew Wisniewski
+ * @author - Period 4
+ * @author - Id 859732
+ *
+ * @author - I received help from Spencer Sharp
+ *
+*/
 import java.util.Scanner;
 
 public class Histogram
@@ -7,7 +16,19 @@ public class Histogram
 
 	//constructor
 
-	//set method
-
-	//toString method
+	//Returns the number of times val is in array
+	public static int getCount(int[] nums, int val)
+	{
+		int count = 0;
+		for (int item : nums)
+		{
+			if (item == val)
+			{
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	//toString
 }
--- Lab14/Lab14e-Histogram/HistogramRunner.java
+++ Lab14/Lab14e-Histogram/HistogramRunner.java
@@ -1,8 +1,36 @@
-
+/**
+ * HistogramRunner.java  10/7/2016
+ *
+ * @author - Matthew Wisniewski
+ * @author - Period 4
+ * @author - Id 859732
+ *
+ * @author - I received help no one
+ *
+*/
 
 public class HistogramRunner
 {
 	public static void main(String args[])
 	{
+		int[] one = {1, 5, 3, 4, 5, 5, 5, 4, 3, 2, 5, 5, 5, 3};
+		for(int i = 0; i <= 9; i++)
+		{
+			System.out.println(i + " = " + Histogram.getCount(one, i));
+		}
+		
+		int[] two = {2, 3, 4, 5, 6, 7, 8, 9, 0, 2, 3, 5, 6, 8, 8, 8, 9, 4, 5};
+		System.out.println("\n");
+		for(int i = 0; i <= 9; i++)
+		{
+			System.out.println(i + " = " + Histogram.getCount(two, i));
+		}
+		
+		int[] three = {2, 3, 4, 5, 6, 7, 8, 2, 0, 2, 3, 5, 6, 8, 8, 8, 9, 4, 5};
+		System.out.println("\n");
+		for(int i = 0; i <= 9; i++)
+		{
+			System.out.println(i + " = " + Histogram.getCount(three, i));
+		}
 	}
 }

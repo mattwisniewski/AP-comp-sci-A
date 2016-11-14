8bacce9e534e45a0f08911c1a423e402ff05c4c3
Matthew Wisniewski <matthew.wisniews@gmail.com>
2016-11-14 15:40:29 -0600

    checkstyled

--- Lab 15/Lab15c-FindNextLargest/NumberSearch.java
+++ Lab 15/Lab15c-FindNextLargest/NumberSearch.java
@@ -5,19 +5,23 @@
  * @author - Period 4
  * @author - Id 859732
  *
- * @author - I received help from no one
+ * @author - I received help from Ryan Rad
  *
 */
-
-import java.util.Arrays;
 
 public class NumberSearch
 {
 	public static int getNextLargest(int[] numArray, int searchNum)
 	{
-		for (int item : numArray)
+		int done = 0;
+		//Goes through array and finds the number less than and the next larger than
+		for (int num : numArray)
 		{
-			if (
+			if (num > searchNum && (num < done || done == 0))
+			{
+				done = num;
+			}
 		}
+		return done;
 	}
 }
